93c0e11634218e4caba92b825f5798b4c1e96b4a
Swati Arya <144891393+arya-s-cmd@users.noreply.github.com>
2025-06-11 22:27:05 +0530

    Update send.py

--- send.py
+++ send.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 
 account_sid = 'AC3b3c5bfe25349fc0d13619377c3c11c1'
 auth_token = '9dcf2e35314b8c740d5d9d555bda6bf1'
-client = Client(account_sid, auth_token) 
+client = Client(account_sid, auth_token)
 
 def sendSms():
     message = client.messages.create(
