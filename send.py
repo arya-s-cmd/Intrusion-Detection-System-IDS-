72ee4fa8f169a442f2896e24bcd001b1e02d3000
Swati Arya <144891393+arya-s-cmd@users.noreply.github.com>
2025-06-11 22:27:25 +0530

    Update send.py

--- send.py
+++ send.py
@@ -4,7 +4,7 @@ account_sid = 'AC3b3c5bfe25349fc0d13619377c3c11c1'
 auth_token = '9dcf2e35314b8c740d5d9d555bda6bf1'
 client = Client(account_sid, auth_token)
 
-def sendSms()::
+def sendSms():
     message = client.messages.create(
         from_='+17623095671',  # update the no.
         body='Alert!! Intrusion Detected.',
